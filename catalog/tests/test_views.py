6b2235ef60f650fe04b1ebb02d0892e6052e9d8a
Wellington Viana <wellvsilva.programador@gmail.com>
2016-10-11 16:51:59 -0300

    ajustando whitenoise no projeto

---
+++ catalog/tests/test_views.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+
+from django.test import TestCase, Client
+from django.core.urlresolvers import reverse
+
+from model_mommy import model_mommy
+
+from catalog.models import Product, Category
+
+
+class ProductListTestCase(TestCase):
+
+    def setUp(self):
+        self.url = reverse('catalog:product_list')
+        self.client = Client()
+        self.products = mommy.make('catalog.Product', _quantity=10)
+
+    def tearDown(self):
+        Product.objects.all().delete()
+
+    def test_view_ok(self):
+        response = self.client.get(self.url)
+        self.assertTrue('product_list' in response.context)
+        product_list = response.context['product_list']
+        self.assertEquals(product_list.count(), 10)
